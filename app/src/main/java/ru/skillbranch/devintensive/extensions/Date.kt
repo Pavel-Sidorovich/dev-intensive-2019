7716e1128a0d13fed4cc33b0be9a92a673102a7e
Pavel-Sidorovich <sidorovichpavelalex@gmail.com>
2019-07-03 16:27:54 +0300

    Add "plural"

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -34,11 +34,11 @@ fun Date.humanizeDiff(date: Date = Date()): String {
             in 0L * SECOND..1L * SECOND -> "только что"
             in 1L * SECOND..45L * SECOND -> "несколько секунд назад"
             in 45L * SECOND..75L * SECOND -> "минуту назад"
-            in 75L * SECOND..45L * MINUTE -> "${TimeUnits.MINUTE.plural(different / MINUTE)} назад"//"${different / MINUTE} ${getNumForm("минуту;минуты;минут" , different / MINUTE)} назад"
+            in 75L * SECOND..45L * MINUTE -> "${TimeUnits.MINUTE.plural(different / MINUTE)} назад"
             in 45L * MINUTE..75L * MINUTE -> "час назад"
-            in 75L * MINUTE..22L * HOUR -> "${TimeUnits.HOUR.plural(different / HOUR)} назад"//"${different / HOUR} ${getNumForm("час;часа;часов" , different / HOUR)} назад"
+            in 75L * MINUTE..22L * HOUR -> "${TimeUnits.HOUR.plural(different / HOUR)} назад"
             in 22L * HOUR..26L * HOUR -> "день назад"
-            in 26L * HOUR..360L * DAY -> "${TimeUnits.DAY.plural(different / DAY)} назад"//"${different / DAY} ${getNumForm("день;дня;дней" , different / DAY)} назад"
+            in 26L * HOUR..360L * DAY -> "${TimeUnits.DAY.plural(different / DAY)} назад"
             else -> "более года назад"
         }
     } else {
@@ -46,26 +46,14 @@ fun Date.humanizeDiff(date: Date = Date()): String {
             in 0L * SECOND..1L * SECOND -> "только что"
             in 1L * SECOND..45L * SECOND -> "через несколько секунд"
             in 45L * SECOND..75L * SECOND -> "через минуту"
-            in 75L * SECOND..45L * MINUTE -> "через ${TimeUnits.MINUTE.plural(absDifferent / MINUTE)}"//${absDifferent / MINUTE} ${getNumForm("минуту;минуты;минут" , absDifferent / MINUTE)}"
+            in 75L * SECOND..45L * MINUTE -> "через ${TimeUnits.MINUTE.plural(absDifferent / MINUTE)}"
             in 45L * MINUTE..75L * MINUTE -> "через час"
-            in 75L * MINUTE..22L * HOUR -> "через ${TimeUnits.HOUR.plural(absDifferent / HOUR)}"//${absDifferent / HOUR} ${getNumForm("час;часа;часов" , absDifferent / HOUR)}"
+            in 75L * MINUTE..22L * HOUR -> "через ${TimeUnits.HOUR.plural(absDifferent / HOUR)}"
             in 22L * HOUR..26L * HOUR -> "через день"
-            in 26L * HOUR..360L * DAY -> "через ${TimeUnits.DAY.plural(absDifferent / DAY)}"//${absDifferent / DAY} ${getNumForm("день;дня;дней" , absDifferent / DAY)}"
+            in 26L * HOUR..360L * DAY -> "через ${TimeUnits.DAY.plural(absDifferent / DAY)}"
             else -> "более чем через год"
         }
     }
-}
-
-private fun getNumForm(pluralForms:String , value: Long):String {
-    val forms = pluralForms.split(";")
-    when (value % 10) {
-        1L -> if (value % 100L != 11L)
-            return forms[0]
-        2L, 3L, 4L -> return if (value % 100 !in 12..14)
-            forms[1]
-        else forms[2]
-    }
-    return forms[2]
 }
 
 enum class TimeUnits {
