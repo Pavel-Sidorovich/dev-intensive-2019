62e27eadd1410c5b64d99577d9017cd3edc96d4d
Pavel-Sidorovich <sidorovichpavelalex@gmail.com>
2019-09-07 22:34:23 +0300

    Update ProfileActivity for testing

--- app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
@@ -54,7 +54,7 @@ class ProfileActivity : AppCompatActivity() {
     }
 
     private fun updateTheme(mode: Int) {
-        delegate.localNightMode = mode
+        delegate.setLocalNightMode(mode)
     }
 
     private fun updateUI(profile: Profile) {
